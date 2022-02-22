739fb31adac72a0c3e3cc9914f137f2f909208bd
liisapikkarainen <liisapikkarainen1@gmail.com>
2022-02-22 09:55:31 +0200

    tehtävälista

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/todo/';
 
 function App() {
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        //console.log(response.data);
+        setTasks(response.data);
+      }).catch(error => {
+        alert(error);
+      });
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <ol>
+        {tasks?.map(task =>(
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
     </div>
   );
 }
