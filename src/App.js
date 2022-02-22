fc2b78c63c11ddcd1dc2b54ab29c840ee7daae49
liisapikkarainen <liisapikkarainen1@gmail.com>
2022-02-22 11:25:15 +0200

    tehtävälista

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const URL = 'http://localhost/todo/';
 
 function App() {
+  const [task, setTask] = useState('');
   const [tasks,setTasks] = useState([]);
 
   useEffect(() => {
@@ -13,12 +14,38 @@ function App() {
         //console.log(response.data);
         setTasks(response.data);
       }).catch(error => {
+        alert(error.response ? error.response.data.error : error); // Yhden rivin vastaava koodi alla olevasta if-koodista
+        /* if (error.response) {
+          alert(error.response.data.error);
+        } else {
         alert(error);
+        } */
       });
   }, [])
   
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task});
+    axios.post(URL + 'add.php',json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data]);
+      setTask('');
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
   return (
     <div className='container'>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={task} placeholder='Add new task' onChange={e => setTask(e.target.value)}></input>
+        <button>Save</button>
+      </form>
       <ol>
         {tasks?.map(task =>(
           <li key={task.id}>{task.description}</li>
